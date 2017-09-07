32fcec3ec90e6b041b4456f43cd242a285fa028d
Ignacio Quintero <ignacio.quintero@yale.edu>
2017-09-07 16:22:33 -0400

    Implemented simulations for Compete given a pylogenetic tree

--- src/sim_utils.jl
+++ src/sim_utils.jl
@@ -11,83 +11,175 @@ t(-_-t)
 
 
 
-bs = 2.1     # branch start
-bf = 4.2     # branch end
-bt = bf - bs # branch time
 
-bδt = bt/1000
-λ1 = 0.3
-λ0 = 0.1
-ω1 = -0.1
-ω0 = 0.1
-δt = 0.01
+tree_file = homedir()*"/data/turnover/example_tree.tre"
 
 
 
 
 
 
-Yt = rand(0:1, 4, 3)
-Xt = rand(4)
+
+"""
+    simulate_compete(X_initial::Float64,  Y_initial::Array{Int64,1}, tree_file::String, ωx::Float64, σ::Float64, λ1::Float64, λ0::Float64, ω1::Float64, ω0::Float64; const_δt = 1e-4)
+
+Simulate biogeographic and trait evolution according to Compete model.
+"""
+function simulate_compete(X_initial::Float64,
+                          Y_initial::Array{Int64,1},
+                          tree_file::String,
+                          ωx       ::Float64,
+                          σ        ::Float64,
+                          λ1       ::Float64,
+                          λ0       ::Float64,
+                          ω1       ::Float64,
+                          ω0       ::Float64;
+                          const_δt = 1e-4)
 
 
 
+  tree, bts = read_tree(tree_file)
 
+  br = branching_times(tree)
 
-# number of species and areas
-n, k = size(Yt)
+  # sort according to branching times
+  brs = sortrows(br, by = x->(x[5]), rev = true)
 
+  # sort branching times
+  sort!(bts, rev = true)
 
-arav = zeros(k)   # area averages
-liav = zeros(n)   # lineage averages
-alλ1 = zeros(n,k)  # area specific lineage rates
-alλ0 = zeros(n,k)  # area specific lineage rates
+  # add present
+  push!(bts, 0.0)
 
+  # number of speciation events
+  nbt = endof(bts)-1
 
-
-
-### start
-
-arav, liav = ar_lin_avg(Xt, Yt, arav, liav, n, k)
-
-alλ1, alλ0 = lin_rates(Xt, arav, alλ1, alλ0, λ1, λ0, ω1, ω0, n, k)
-
-
-
-### step
-
-# biogeographic step
-
-
-
-Ytn = copy(Yt)
-biogeosam_1step(alλ1, alλ0, δt, Yt, n, k)
-
-
-
-
-
-  for i in eachindex(λ1v)
-
-    vp = copy(vi)
-    vr = biogeosam_1step(λ1v[i], λ0v[i], bδt[i], vp)
-
-    while check_sam(vr)
-      vr = biogeosam_1step(λ1v[i], λ0v[i], bδt[i], vp)
-    end
-
-    vi = vr[1]
+  # calculate speciation waiting times
+  swt = Array{Float64,1}(nbt)
+  for i in Base.OneTo(nbt)
+    swt[i] = bts[i] - bts[i+1]
   end
 
+  # initial values
+  Xt = fill(X_initial, 2)
 
-# trait step
+  Y_initial = reshape(Y_initial, 1, :)
+  Yt = vcat(Y_initial, Y_initial)
+
+  # start of alive
+  alive  = sortrows(br, by = x->(x[1]))[1:2,2]
+  nalive = length(alive)
+
+  # loop through waiting times
+  for j in Base.OneTo(nbt)
+
+    nreps = reps_per_period(swt[j], const_δt)
+
+    # simulate durin the waiting time
+    Xt, Yt = branch_sim(Xt, Yt, nreps, δt, ωx, σ, λ1, λ0, ω1, ω0)
+
+    if j == nbt
+      break
+    end
+
+    # which speciates
+    wsp = brs[j,2]
+
+    # where to insert
+    wti = find(wsp .== alive)[1]
+
+    # index to insert
+    idx = sort(push!(collect(1:nalive), wti))
+
+    # insert in Xt & Yt
+    Xt = Xt[idx]
+    Yt = Yt[idx,:]
+
+    # update alive
+    chs = brs[find(wsp .== brs[:,1]),2]
+
+    alive[wti] = chs[1]
+    insert!(alive, wti+1, chs[2])
+
+    nalive = length(alive)
+
+  end
+
+  tip_traits = Dict(convert(Int64, alive[i]) => Xt[i] for i = 1:nalive)
+  tip_ranges = Dict(convert(Int64, alive[i]) => Yt[i,:] for i = 1:nalive)
+
+  return tip_traits, tip_ranges
+
+end
 
 
-# repeat
 
 
 
-check_sam(Ytc::Array{Int64,2}, nch::Array{Int64,1}, n::Int64, k::Int64)
+"""
+    reps_per_period(br_length::Float64, const_δt::Float64)
+
+Estimate number of repetitions for each speciation waiting time.
+"""
+reps_per_period(br_length::Float64, const_δt::Float64) = 
+  round(Int64,cld(br_length,const_δt))
+
+
+
+
+
+"""
+    branch_sim(Xt::Array{Float64,1}, Yt::Array{Int64,2}, br_δts::Array{Float64,1})
+
+Simulate biogeographic and trait evolution in a branch.
+"""
+function branch_sim(Xt    ::Array{Float64,1}, 
+                    Yt    ::Array{Int64,2},
+                    nreps ::Int64,
+                    δt    ::Float64,
+                    ωx    ::Float64, 
+                    σ     ::Float64, 
+                    λ1    ::Float64, 
+                    λ0    ::Float64, 
+                    ω1    ::Float64, 
+                    ω0    ::Float64)
+
+  # n species and k areas
+  const n, k = size(Yt)
+
+  # allocate memory for area and lineage averages 
+  arav = zeros(k)    # area averages
+  liav = zeros(n)    # lineage averages
+  alλ1 = zeros(n,k)  # area specific lineage rates
+  alλ0 = zeros(n,k)  # area specific lineage rates
+
+  for i in Base.OneTo(nreps)
+
+    # estimate area and lineage averages
+    arav, liav = ar_lin_avg(Xt, Yt, arav, liav, n, k)
+
+    # estimate area colonization/loss rates
+    alλ1, alλ0 = lin_rates(Xt, arav, alλ1, alλ0, λ1, λ0, ω1, ω0, n, k)
+
+    # trait step
+    traitsam_1step(Xt, liav, δt, ωx, σ, n)
+
+    # biogeographic step
+    Ytn = copy(Yt)
+    nch = biogeosam_1step(alλ1, alλ0, δt, Ytn, n, k)
+
+    while check_sam(Ytn, nch, n, k)
+      Ytn = copy(Yt)
+      nch = biogeosam_1step(alλ1, alλ0, δt, Ytn, n, k)
+    end
+
+    Yt = Ytn
+  end
+
+  return Xt, Yt
+end
+
+
 
 
 
@@ -184,13 +276,25 @@ end
 
 
 """
-    traitsam_1step(xi::Float64, μ::Float64, δt::Float64, ωx::Float64, σ::Float64)
+    traitsam_1step((Xt::Array{Float64,1}, μ ::Array{Float64,1}, δt::Float64, ωx::Float64, σ::Float64, n::Int64)
 
 Sample one step for trait evolution history: X(t + δt).
 """
-traitsam_1step(xi::Float64, μ::Float64, δt::Float64, ωx::Float64, σ::Float64) = 
-  xi += ωx*(μ - xi)*δt + randn()*σ*sqrt(δt)
+function traitsam_1step(Xt::Array{Float64,1}, 
+                        μ ::Array{Float64,1}, 
+                        δt::Float64, 
+                        ωx::Float64, 
+                        σ ::Float64,
+                        n ::Int64)
 
+  @inbounds @fastmath begin
+    
+    for i in Base.OneTo(n)
+      Xt[i] += ωx*(μ[i] - Xt[i])*δt + randn()*σ*sqrt(δt)
+    end
+  end
+
+end
 
 
 
@@ -226,7 +330,7 @@ function biogeosam_1step(alλ1 ::Array{Float64,2},
 
   end
 
-  return Ytn, nch
+  return nch
 end
 
 
