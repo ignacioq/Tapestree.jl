59072b6f66524b8a304f4af0cd807a321c9f3a1f
Ignacio Quintero <ignacio.quintero@yale.edu>
2019-03-28 18:25:29 +0100

    higher efficiency by preallocating, but nee dot control for shared memeory

--- src/slice_sampler_utils.jl
+++ src/slice_sampler_utils.jl
@@ -24,26 +24,29 @@ function w_sampler(lhf      ::Function,
                    npars    ::Int64,
                    optimal_w::Float64)
 
-  w = fill(1.0, npars)
+  w  = ones(npars)
+  ps = Array{Float64,2}(undef, 100, npars)
 
-  ps = zeros(Float64, 100, npars)
+  # posterior
+  hc = lhf(p)
 
-  hc::Float64 = lhf(p)
+  # preallocate pp
+  pp = Array{Float64,1}(undef, npars)
 
   prog = Progress(100, 5, "estimating optimal widths...", 20)
 
   for it in Base.OneTo(100)
 
     for j in nnps
-      S     = (hc - Random.randexp())::Float64
-      L, R  = find_nonneg_int(p, j, S, lhf, w[j])::NTuple{2,Float64}
-      p, hc = sample_int(p, j, L, R, S, lhf)::Tuple{Array{Float64,1},Float64}
+      S     = (hc - Random.randexp())
+      L, R  = find_nonneg_int(p, pp, j, S, lhf, w[j])
+      p, hc = sample_int(p, pp, j, L, R, S, lhf)
     end
 
     for j in nps
-      S     = (hc - Random.randexp())::Float64
-      L, R  = find_real_int(p, j, S, lhf, w[j])::NTuple{2,Float64}
-      p, hc = sample_int(p, j, L, R, S, lhf)::Tuple{Array{Float64,1},Float64}
+      S     = (hc - Random.randexp())
+      L, R  = find_real_int(p, pp, j, S, lhf, w[j])
+      p, hc = sample_int(p, pp, j, L, R, S, lhf)
     end
 
     @inbounds setindex!(ps, p, it, :)
@@ -61,83 +64,48 @@ end
 
 
 
-
 """
-    w_sampler(lhf, p::Array{Float64,1}, pupd::Array{Int64,1})
-
-Run 100 iterations of the sampler to estimate appropriate w's.
-"""
-function w_sampler(lhf      ::Function, 
-                   p        ::Array{Float64,1},
-                   nnps     ::Array{Int64,1},
-                   npars    ::Int64,
-                   optimal_w::Float64)
-
-  w = fill(1.0, npars)
-
-  ps = zeros(Float64,100, npars)
-
-  hc::Float64 = lhf(p)
-
-  prog = Progress(100, 5, "estimating optimal w widths...", 20)
-
-  for it in Base.OneTo(100)
-
-    for j in nnps
-      S     = (hc - Random.randexp())::Float64
-      L, R  = find_nonneg_int(p, j, S, lhf, w[j])::NTuple{2,Float64}
-      p, hc = sample_int(p, j, L, R, S, lhf)::Tuple{Array{Float64,1},Float64}
-    end
-
-    @inbounds setindex!(ps, p, it, :)
-
-    next!(prog)
-  end
-
-  w = optimal_w .* (reduce(max, ps, dims=1) .- reduce(min, ps, dims=1))
-  w = reshape(w, size(w,2))
-
-  return (p, w)::Tuple{Array{Float64,1},Array{Float64,1}}
-end
-
-
-
-
-
-
-
-"""
-    find_nonneg_int(p::Array{Float64}, j::Int64, S::Float64, postf, w::Float64)
+    find_nonneg_int(p    ::Array{Float64}, 
+                    pp   ::Array{Float64},
+                    j    ::Int64, 
+                    S    ::Float64, 
+                    postf::Function, 
+                    w    ::Float64)
 
 Estimate a non_negative slice interval.
 """
-function find_nonneg_int(p::Array{Float64}, j::Int64, S::Float64, postf, w::Float64)
+function find_nonneg_int(p    ::Array{Float64}, 
+                         pp   ::Array{Float64},
+                         j    ::Int64, 
+                         S    ::Float64, 
+                         postf::Function, 
+                         w    ::Float64)
 
-  pc::Array{Float64,1} = deepcopy(p)
+  copyto!(pp, p)
 
-  L::Float64 = pc[j] - w*rand()
+  L::Float64 = pp[j] - w*rand()
   R::Float64 = L + w
 
   if L <= 0.0
-    L = 1e-100
+    L = 1e-30
   end
 
   # left extreme
-  pc[j] = L::Float64
-  while S < postf(pc)
+  pp[j] = L::Float64
+  while S < postf(pp)
     L -= w::Float64
     if L <= 0.0
-      L = 1e-100
+      L = 1e-30
       break
     end
-    pc[j] = L::Float64
+    pp[j] = L::Float64
   end
 
   # right extreme
-  pc[j] = R::Float64
-  while S < postf(pc)
+  pp[j] = R::Float64
+  while S < postf(pp)
     R    += w::Float64
-    pc[j] = R::Float64
+    pp[j] = R::Float64
   end
 
   return (L, R)::NTuple{2,Float64}
@@ -148,29 +116,39 @@ end
 
 
 """
-    find_real_int(p::Array{Float64}, j::Int64, S::Float64, postf, w::Float64)
+    find_real_int(p    ::Array{Float64}, 
+                  pp   ::Array{Float64}, 
+                  j    ::Int64, 
+                  S    ::Float64, 
+                  postf::Function, 
+                  w    ::Float64)
 
 Estimate a non_negative slice interval.
 """
-function find_real_int(p::Array{Float64}, j::Int64, S::Float64, postf, w::Float64)
+function find_real_int(p    ::Array{Float64}, 
+                       pp   ::Array{Float64}, 
+                       j    ::Int64, 
+                       S    ::Float64, 
+                       postf::Function, 
+                       w    ::Float64)
 
-  pc::Array{Float64,1} = deepcopy(p)
+  copyto!(pp, p)
 
-  L::Float64 = pc[j] - w*rand()
+  L::Float64 = pp[j] - w*rand()
   R::Float64 = L + w
 
   # left extreme
-  pc[j] = L::Float64
-  while S < postf(pc)
+  pp[j] = L::Float64
+  while S < postf(pp)
     L    -= w::Float64
-    pc[j] = L::Float64
+    pp[j] = L::Float64
   end
 
   # right extreme
-  pc[j] = R::Float64
-  while S < postf(pc)
+  pp[j] = R::Float64
+  while S < postf(pp)
     R    += w::Float64
-    pc[j] = R::Float64
+    pp[j] = R::Float64
   end
 
   return (L, R)::NTuple{2,Float64}
@@ -180,37 +158,41 @@ end
 
 
 
-
 """
-    sample_int(p::Array{Float64}, j::Int64, L::Float64, R::Float64, S::Float64, postf)
+    sample_int(p    ::Array{Float64,1}, 
+               pp   ::Array{Float64,1},
+               j    ::Int64, 
+               L    ::Float64, 
+               R    ::Float64, 
+               S    ::Float64, 
+               postf::Function)
 
 Take one sample within the interval of the slice.
 """
-function sample_int(p::Array{Float64}, 
-                    j::Int64, 
-                    L::Float64, 
-                    R::Float64, 
-                    S::Float64, 
-                    postf)
+function sample_int(p    ::Array{Float64,1}, 
+                    pp   ::Array{Float64,1},
+                    j    ::Int64, 
+                    L    ::Float64, 
+                    R    ::Float64, 
+                    S    ::Float64, 
+                    postf::Function)
 
   @inbounds begin
+    copyto!(pp, p)
 
-    pc::Array{Float64,1} = deepcopy(p)
-
-    pc[j] = (L + rand()*(R-L))::Float64
-    
     while true
-      pc[j] = (L + rand()*(R-L))::Float64
+      pp[j] = (L + rand()*(R-L))::Float64
 
-      postc = postf(pc)::Float64
+      postc = postf(pp)::Float64
       if S < postc
-        return (pc, postc)::Tuple{Array{Float64,1}, Float64}
+        p[j] = pp[j]
+        return (p, postc)::Tuple{Array{Float64,1}, Float64}
       end
 
-      if pc[j] < p[j]
-        L = pc[j]::Float64
+      if pp[j] < p[j]
+        L = pp[j]::Float64
       else
-        R = pc[j]::Float64
+        R = pp[j]::Float64
       end
     end
 
