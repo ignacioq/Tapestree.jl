3a95c46613cc5b7134a326ea75ae8a0286fc506d
Ignacio Quintero <ignacio.quintero@yale.edu>
2019-05-21 12:09:19 +0200

    aesthetic change

--- src/tribe_utils.jl
+++ src/tribe_utils.jl
@@ -99,10 +99,10 @@ function Ptrfast(λ1::Float64,
                  λ0::Float64, 
                  t::Float64)
 
-    ex   ::Float64 = exp(-(λ1 + λ0)*t)
-    sd1  ::Float64 = 1.0/(λ1 + λ0)
-    λ1ex ::Float64 = λ1*ex
-    λ0ex ::Float64 = λ0*ex
+    ex  ::Float64 = exp(-(λ1 + λ0)*t)
+    sd1 ::Float64 = 1.0/(λ1 + λ0)
+    λ1ex::Float64 = λ1*ex
+    λ0ex::Float64 = λ0*ex
 
     ((sd1*(λ0 + λ1ex), sd1*(λ1 - λ1ex)), 
      (sd1*(λ0 - λ0ex), sd1*(λ1 + λ0ex)))
@@ -125,9 +125,9 @@ function Ptrfast_start(λ1   ::Float64,
                        t    ::Float64, 
                        ::Type{Val{0}})
 
-  ex   ::Float64 = exp(-(λ1 + λ0)*t)
-  sd1  ::Float64 = 1.0/(λ1 + λ0)
-  λ1ex ::Float64 = λ1*ex
+  ex  ::Float64 = exp(-(λ1 + λ0)*t)
+  sd1 ::Float64 = 1.0/(λ1 + λ0)
+  λ1ex::Float64 = λ1*ex
 
   return sd1*(λ0 + λ1ex), sd1*(λ1 - λ1ex) 
 end
@@ -149,9 +149,9 @@ function Ptrfast_start(λ1   ::Float64,
                        t    ::Float64, 
                        ::Type{Val{1}})
 
-  ex   ::Float64 = exp(-(λ1 + λ0)*t)
-  sd1  ::Float64 = 1.0/(λ1 + λ0)
-  λ0ex ::Float64 = λ0*ex
+  ex  ::Float64 = exp(-(λ1 + λ0)*t)
+  sd1 ::Float64 = 1.0/(λ1 + λ0)
+  λ0ex::Float64 = λ0*ex
 
   return sd1*(λ0 - λ0ex), sd1*(λ1 + λ0ex)
 end
@@ -172,8 +172,8 @@ function Ptrfast_end(λ1   ::Float64,
                      t    ::Float64, 
                      ::Type{Val{0}})
 
-  ex   ::Float64 = exp(-(λ1 + λ0)*t)
-  sd1  ::Float64 = 1.0/(λ1 + λ0)
+  ex ::Float64 = exp(-(λ1 + λ0)*t)
+  sd1::Float64 = 1.0/(λ1 + λ0)
 
   return sd1*(λ0 + λ1*ex), sd1*(λ0 - λ0*ex)
 end
@@ -193,8 +193,8 @@ function Ptrfast_end(λ1   ::Float64,
                      t    ::Float64, 
                      ::Type{Val{1}})
 
-  ex   ::Float64 = exp(-(λ1 + λ0)*t)
-  sd1  ::Float64 = 1.0/(λ1 + λ0)
+  ex ::Float64 = exp(-(λ1 + λ0)*t)
+  sd1::Float64 = 1.0/(λ1 + λ0)
 
   return sd1*(λ1 - λ1*ex), sd1*(λ1 + λ0*ex)
 end
