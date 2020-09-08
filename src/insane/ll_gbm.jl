589d1683de4e79baed894fdb15b42507d178d92a
Ignacio Quintero <ignacioquinterom@gmail.com>
2020-09-08 12:20:41 -0500

    log-likelihood for gbm implemented

--- src/insane/ll_gbm.jl
+++ src/insane/ll_gbm.jl
@@ -11,3 +11,66 @@ Created 03 09 2020
 
 
 
+
+"""
+    llik_gbm(tree::iTgbm, 
+             σ_λ ::Float64, 
+             σ_μ ::Float64)
+
+Returns the log-likelihood for a `iTgbm` according to GBM birth-death.
+"""
+function llik_gbm(tree::iTgbm, 
+                  σ_λ ::Float64, 
+                  σ_μ ::Float64)
+
+  tsb = ts(tree)
+  lλb = lλ(tree)
+  lμb = lμ(tree)
+
+  if istip(tree) 
+    ll_gbm_b(tsb, lλb, lμb, σ_λ, σ_μ) + (isextinct(tree) ? lμb[end] : 0.0)
+  else
+    ll_gbm_b(tsb, lλb, lμb, σ_λ, σ_μ)  + (2.0*lλb[end])
+    llik_gbm(tree.d1::iTgbm, σ_λ, σ_μ) + 
+    llik_gbm(tree.d2::iTgbm, σ_λ, σ_μ)
+  end
+end
+
+
+
+
+"""
+    ll_gbm_b(t  ::Array{Float64,1},
+             lλv::Array{Float64,1},
+             lμv::Array{Float64,1},
+             σ_λ::Float64,
+             σ_μ::Float64)
+
+Returns the log-likelihood for a branch according to GBM birth-death.
+"""
+function ll_gbm_b(t  ::Array{Float64,1},
+                  lλv::Array{Float64,1},
+                  lμv::Array{Float64,1},
+                  σ_λ::Float64,
+                  σ_μ::Float64)
+
+  ll   = 0.0
+  ti   = t[1]
+  lλvi = lλv[1]
+  lμvi = lμv[1]
+  for i in 2:lastindex(t)
+    ti1   = t[i]
+    δt    = ti1-ti
+    lλvi1 = lλv[i]
+    lμvi1 = lμv[i]
+    ll   += logdnorm_tc(lλvi1, lλvi, δt*σ_λ) + 
+            logdnorm_tc(lμvi1, lμvi, δt*σ_μ) - 
+            δt*(exp(geoavg2(lλvi, lλvi1)) + exp(geoavg2(lμvi, lμvi1)))
+    lλvi = lλvi1
+    lμvi = lμvi1
+    ti   = ti1 
+  end
+
+  return ll
+end
+
