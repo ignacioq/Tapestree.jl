2d2f211824e543e906a6f79dea8a2338a9bc3688
Ignacio Quintero <ignacioquinterom@gmail.com>
2021-02-25 16:24:44 +0100

    finished forward simulation for cbd

--- src/insane/mcmc_cbd_fs.jl
+++ src/insane/mcmc_cbd_fs.jl
@@ -1,6 +1,6 @@
 #=
 
-constant birth-death MCMC forward simulation
+constant birth-death MCMC using forward simulation
 
 Ignacio Quintero Mächler
 
@@ -11,43 +11,45 @@ Created 25 08 2020
 
 
 
+
 """
-    insane_cbd(tree    ::sTbd, 
-               out_file::String;
-               λprior  ::Float64           = 0.1,
-               μprior  ::Float64           = 0.1,
-               niter   ::Int64             = 1_000,
-               nthin   ::Int64             = 10,
-               nburn   ::Int64             = 200,
-               tune_int::Int64             = 100,
-               ϵi      ::Float64           = 0.4,
-               λi      ::Float64           = NaN,
-               μi      ::Float64           = NaN,
-               λtni    ::Float64           = 1.0,
-               μtni    ::Float64           = 1.0,
-               obj_ar  ::Float64           = 0.4,
-               pupdp   ::NTuple{4,Float64} = (0.4,0.4,0.1,0.1),
-               prints  ::Int64              = 5)
+    insane_cbd_fs(tree    ::sTbd, 
+                  out_file::String;
+                  λprior  ::Float64           = 0.1,
+                  μprior  ::Float64           = 0.1,
+                  niter   ::Int64             = 1_000,
+                  nthin   ::Int64             = 10,
+                  nburn   ::Int64             = 200,
+                  tune_int::Int64             = 100,
+                  ϵi      ::Float64           = 0.4,
+                  λi      ::Float64           = NaN,
+                  μi      ::Float64           = NaN,
+                  ntry    ::Int64             = 20,
+                  λtni    ::Float64           = 1.0,
+                  μtni    ::Float64           = 1.0,
+                  obj_ar  ::Float64           = 0.4,
+                  pupdp   ::NTuple{3,Float64} = (0.4,0.4,0.1),
+                  prints  ::Int64              = 5)
 
 Run insane for constant pure-birth.
 """
-function insane_cbd(tree    ::sTbd, 
-                    out_file::String;
-                    λprior  ::Float64           = 0.1,
-                    μprior  ::Float64           = 0.1,
-                    niter   ::Int64             = 1_000,
-                    nthin   ::Int64             = 10,
-                    nburn   ::Int64             = 200,
-                    tune_int::Int64             = 100,
-                    ϵi      ::Float64           = 0.4,
-                    λi      ::Float64           = NaN,
-                    μi      ::Float64           = NaN,
-                    ntry    ::Int64             = 20,
-                    λtni    ::Float64           = 1.0,
-                    μtni    ::Float64           = 1.0,
-                    obj_ar  ::Float64           = 0.4,
-                    pupdp   ::NTuple{4,Float64} = (0.4,0.4,0.1,0.1),
-                    prints  ::Int64              = 5)
+function insane_cbd_fs(tree    ::sTbd, 
+                       out_file::String;
+                       λprior  ::Float64           = 0.1,
+                       μprior  ::Float64           = 0.1,
+                       niter   ::Int64             = 1_000,
+                       nthin   ::Int64             = 10,
+                       nburn   ::Int64             = 200,
+                       tune_int::Int64             = 100,
+                       ϵi      ::Float64           = 0.4,
+                       λi      ::Float64           = NaN,
+                       μi      ::Float64           = NaN,
+                       ntry    ::Int64             = 20,
+                       λtni    ::Float64           = 1.0,
+                       μtni    ::Float64           = 1.0,
+                       obj_ar  ::Float64           = 0.4,
+                       pupdp   ::Array{Float64,1}  = [0.4,0.4,0.1],
+                       prints  ::Int64             = 5)
 
   # tree characters
   th = treeheight(tree)
@@ -69,22 +71,18 @@ function insane_cbd(tree    ::sTbd,
   bit = BitArray{1}()
   makeiBf!(tree, idf, bit)
 
-  # create wbf vector
-  wbf = falses(lastindex(idf))
-
   # make survival conditioning function (stem or crown)
   svf = iszero(pe(tree)) ? crown_prob_surv_cbd :
                            stem_prob_surv_cbd
 
   # adaptive phase
-  llc, prc, tree, λc, μc, λtn, μtn, idf, dabr = 
-      mcmc_burn_cbd(tree, n, th, tune_int, λprior, μprior, 
-        nburn, ϵi, λi, μi, λtni, μtni, scalef, idf, wbf, dabr, pup, pupdp, 
-        prints, svf)
+  llc, prc, tree, λc, μc, λtn, μtn = 
+      mcmc_burn_cbd(tree, n, th, tune_int, λprior, μprior, ntry,
+        nburn, ϵi, λi, μi, λtni, μtni, scalef, idf, pup, prints, svf)
 
   # mcmc
-  R, tree = mcmc_cbd(tree, llc, prc, λc, μc, λprior, μprior,
-        niter, nthin, λtn, μtn, th, idf, wbf, dabr, pup, pupdp, prints, svf)
+  R, tree = mcmc_cbd(tree, llc, prc, λc, μc, λprior, μprior, ntry,
+        niter, nthin, λtn, μtn, th, idf, pup, prints, svf)
 
   pardic = Dict(("lambda"      => 1),
                 ("mu"          => 2), 
@@ -101,23 +99,25 @@ end
 
 """
     mcmc_burn_cbd(tree    ::sTbd,
-                  tl      ::Float64,
-                  nt      ::Int64,
+                  n       ::Int64,
                   th      ::Float64,
                   tune_int::Int64,
                   λprior  ::Float64,
                   μprior  ::Float64,
                   nburn   ::Int64,
+                  ϵi      ::Float64,
+                  λi      ::Float64,
+                  μi      ::Float64,
                   λtni    ::Float64, 
                   μtni    ::Float64, 
                   scalef  ::Function,
-                  λprior  ::Float64,
-                  μprior  ::Float64,
                   idf     ::Array{iBf,1},
-                  wbf     ::BitArray{1},
-                  dabr    ::Array{Int64,1})
+                  pup     ::Array{Int64,1}, 
+                  prints  ::Int64,
+                  svf     ::Function)
 
-adaptive MCMC phase for da chain for constant birth-death.
+Adaptive MCMC phase for da chain for constant birth-death using forward
+simulation.
 """
 function mcmc_burn_cbd(tree    ::sTbd,
                        n       ::Int64,
@@ -125,6 +125,7 @@ function mcmc_burn_cbd(tree    ::sTbd,
                        tune_int::Int64,
                        λprior  ::Float64,
                        μprior  ::Float64,
+                       ntry    ::Int64,
                        nburn   ::Int64,
                        ϵi      ::Float64,
                        λi      ::Float64,
@@ -133,10 +134,7 @@ function mcmc_burn_cbd(tree    ::sTbd,
                        μtni    ::Float64, 
                        scalef  ::Function,
                        idf     ::Array{iBf,1},
-                       wbf     ::BitArray{1},
-                       dabr    ::Array{Int64,1},
                        pup     ::Array{Int64,1}, 
-                       pupdp   ::NTuple{4,Float64},
                        prints  ::Int64,
                        svf     ::Function)
 
@@ -159,11 +157,6 @@ function mcmc_burn_cbd(tree    ::sTbd,
 
   pbar = Progress(nburn, prints, "burning mcmc...", 20)
 
-
-"""
-here check if forward simulation it works
-"""
-
   for it in Base.OneTo(nburn)
 
     shuffle!(pup)
@@ -184,8 +177,7 @@ here check if forward simulation it works
       
       # forward simulation proposal proposal
       if p === 3
-        bi = rand(idf)
-        tree, llc = fsp(tree, bi, llc, λc, μc, ntry)
+        tree, llc = fsp(tree, rand(idf), llc, λc, μc, ntry)
       end
 
       # log tuning parameters
@@ -200,7 +192,7 @@ here check if forward simulation it works
     next!(pbar)
   end
 
-  return llc, prc, tree, λc, μc, λtn, μtn, idf, dabr
+  return llc, prc, tree, λc, μc, λtn, μtn
 end
 
 
@@ -221,10 +213,11 @@ end
              μtn   ::Float64, 
              th    ::Float64,
              idf   ::Array{iBf,1},
-             wbf   ::BitArray{1},
-             dabr  ::Array{Int64,1})
+             pup   ::Array{Int64,1}, 
+             prints::Int64,
+             svf   ::Function)
 
-MCMC da chain for constant birth-death.
+MCMC da chain for constant birth-death using forward simulation.
 """
 function mcmc_cbd(tree  ::sTbd,
                   llc   ::Float64,
@@ -233,16 +226,14 @@ function mcmc_cbd(tree  ::sTbd,
                   μc    ::Float64,
                   λprior::Float64,
                   μprior::Float64,
+                  ntry  ::Int64,
                   niter ::Int64,
                   nthin ::Int64,
                   λtn   ::Float64,
                   μtn   ::Float64, 
                   th    ::Float64,
                   idf   ::Array{iBf,1},
-                  wbf   ::BitArray{1},
-                  dabr  ::Array{Int64,1},
                   pup   ::Array{Int64,1}, 
-                  pupdp ::NTuple{4,Float64},
                   prints::Int64,
                   svf   ::Function)
 
@@ -261,25 +252,25 @@ function mcmc_cbd(tree  ::sTbd,
     for p in pup
 
       # λ proposal
-      if p === 1
-        llc, prc, λc = λp(tree, llc, prc, λc, lac, λtn, μc, λprior, th, svf)
+      if p == 1
+        llc, prc, λc = λp(tree, llc, prc, λc, λtn, μc, λprior, th, svf)
       end
 
       # μ proposal
-      if p === 2
-        llc, prc, μc = μp(tree, llc, prc, μc, lac, μtn, λc, μprior, th, svf)
+      if p == 2
+        llc, prc, μc = μp(tree, llc, prc, μc, μtn, λc, μprior, th, svf)
       end
-      
+
       # forward simulation proposal proposal
       if p === 3
-        bi = rand(idf)
-        tree, llc = fsp(tree, bi, llc, λc, μc, ntry)
+        tree, llc = fsp(tree, rand(idf), llc, λc, μc, ntry)
       end
 
     end
     # log parameters
     lthin += 1
     if lthin == nthin
+
       lit += 1
       @inbounds begin
         R[lit,1] = Float64(lit)
@@ -375,10 +366,6 @@ end
 
 
 
-bi = rand(idf)
-tree, llc = fsp(tree, bi, llc, λc, μc, ntry)
-
-
 
 """
     fsp(tree::sTbd,
@@ -416,7 +403,7 @@ function fsp(tree::sTbd,
     llr = llik_cbd(t0, λc, μc) + iλ - 
           br_ll_cbd(tree, λc, μc, dri, ldr, 0)
 
-    if -randexp() < iλ
+    if -randexp() < 0.0
       llc += llr
       # swap branch
       tree = swapbranch!(tree, t0, dri, ldr, itb, 0)
@@ -673,194 +660,5 @@ end
 
 
 
-
-
-"""
-    λp(tree  ::sTbd,
-       llc   ::Float64,
-       prc   ::Float64,
-       λc    ::Float64,
-       lac   ::Array{Float64,1},
-       λtn   ::Float64,
-       μc    ::Float64,
-       λprior::Float64)
-
-`λ` proposal function for constant birth-death in adaptive phase.
-"""
-function λp(tree  ::sTbd,
-            llc   ::Float64,
-            prc   ::Float64,
-            λc    ::Float64,
-            lac   ::Array{Float64,1},
-            λtn   ::Float64,
-            μc    ::Float64,
-            λprior::Float64,
-            th    ::Float64,
-            svf   ::Function)
-
-    λp = mulupt(λc, λtn)::Float64
-
-    llp = llik_cbd(tree, λp, μc) - svf(λp, μc, th)
-    prr = llrdexp_x(λp, λc, λprior)
-
-    if -randexp() < (llp - llc + prr + log(λp/λc))
-      llc     = llp::Float64
-      prc    += prr::Float64
-      λc      = λp::Float64
-      lac[1] += 1.0
-    end
-
-    return llc, prc, λc
-end
-
-
-
-
-"""
-    λp(tree  ::sTbd,
-       llc   ::Float64,
-       prc   ::Float64,
-       λc    ::Float64,
-       λtn   ::Float64,
-       μc    ::Float64,
-       λprior::Float64)
-
-`λ` proposal function for constant birth-death.
-"""
-function λp(tree  ::sTbd,
-            llc   ::Float64,
-            prc   ::Float64,
-            λc    ::Float64,
-            λtn   ::Float64,
-            μc    ::Float64,
-            λprior::Float64,
-            th    ::Float64,
-            svf   ::Function)
-
-    λp = mulupt(λc, λtn)::Float64
-
-    llp = llik_cbd(tree, λp, μc) - svf(λp, μc, th)
-    prr = llrdexp_x(λp, λc, λprior)
-
-    if -randexp() < (llp - llc + prr + log(λp/λc))
-      llc     = llp::Float64
-      prc    += prr::Float64
-      λc      = λp::Float64
-    end
-
-    return llc, prc, λc 
-end
-
-
-
-
-"""
-    μp(tree  ::sTbd,
-       llc   ::Float64,
-       prc   ::Float64,
-       μc    ::Float64,
-       lac   ::Array{Float64,1},
-       μtn   ::Float64,
-       λc    ::Float64,
-       μprior::Float64)
-
-`μ` proposal function for constant birth-death in adaptive phase.
-"""
-function μp(tree  ::sTbd,
-            llc   ::Float64,
-            prc   ::Float64,
-            μc    ::Float64,
-            lac   ::Array{Float64,1},
-            μtn   ::Float64,
-            λc    ::Float64,
-            μprior::Float64,
-            th    ::Float64,
-            svf   ::Function)
-
-    μp = mulupt(μc, rand() < 0.3 ? μtn : 4.0*μtn)::Float64
-
-    # one could make a ratio likelihood function
-    sc  = svf(λc, μp, th)
-    llp = sc == -Inf ? -Inf : llik_cbd(tree, λc, μp) - sc
-
-    prr = llrdexp_x(μp, μc, μprior)
-
-    if -randexp() < (llp - llc + prr + log(μp/μc))
-      llc  = llp::Float64
-      prc += prr::Float64
-      μc   = μp::Float64
-      lac[2] += 1.0
-    end
-
-    return llc, prc, μc 
-end
-
-
-
-
-"""
-    μp(tree  ::sTbd,
-       llc   ::Float64,
-       prc   ::Float64,
-       μc    ::Float64,
-       μtn   ::Float64,
-       λc    ::Float64,
-       μprior::Float64)
-
-`μ` proposal function for constant birth-death.
-"""
-function μp(tree  ::sTbd,
-            llc   ::Float64,
-            prc   ::Float64,
-            μc    ::Float64,
-            μtn   ::Float64,
-            λc    ::Float64,
-            μprior::Float64,
-            th    ::Float64,
-            svf   ::Function)
-
-    μp = mulupt(μc, rand() < 0.3 ? μtn : 4.0*μtn)::Float64
-
-    # one could make a ratio likelihood function
-    sc  = svf(λc, μp, th)
-    llp = sc == -Inf ? -Inf : llik_cbd(tree, λc, μp) - sc
-
-    prr = llrdexp_x(μp, μc, μprior)
-
-    if -randexp() < (llp - llc + prr + log(μp/μc))
-      llc  = llp::Float64
-      prc += prr::Float64
-      μc   = μp::Float64
-    end
-
-    return llc, prc, μc 
-end
-
-
-
-
-"""
-    λμprop()
-
-Generate proportional proposals for `λ` and `μ`
-using random samples from **LogNormal** distributions. 
-"""
-function λμprop() 
-
-  lg = lnr()
-
-  return lg*exp(randn()*0.3 - 0.044),
-         lg*exp(randn()*0.3 - 0.044) 
-end
-
-
-
-
-"""
-    lnr()
-
-**LogNormal** random samples with median of `1`. 
-"""
-lnr() = @fastmath exp(randn())
 
 
