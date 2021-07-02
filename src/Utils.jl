494487ec8689a0ee6f2637a1889d682a6940585d
Ignacio Quintero <ignacioquinterom@gmail.com>
2021-07-02 17:49:29 +0200

    export rtree utilities

--- src/Utils.jl
+++ src/Utils.jl
@@ -14,7 +14,8 @@ include("utils/tree_utils.jl")
 include("utils/density_functions.jl")
 
 export rtree, read_tree, make_ape_tree, maketriads, abs_time_branches,
-  brts, tree_height, postorderedges, logdexp, logdunifU, logdunif, llrdexp_x,
+  brts, tree_height, postorderedges, remove_extinct, numberedges, 
+  logdexp, logdunifU, logdunif, llrdexp_x,
   logdbeta, llrdbeta_x, logdnorm, logdnorm_tc, llrdnorm_ωxm, llrdnorm_σ², 
   llrdnorm_μ, llrdnorm_x, llrdnorm_xμ, logdtnorm, llrdtnorm_x, erf_custom,
   logdhcau, logdhcau1
