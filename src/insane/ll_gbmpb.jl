c27dc9b267f8aeaa3fa36fb4056735d1ef85fcad
Ignacio Quintero <ignacioquinterom@gmail.com>
2021-08-10 12:38:25 +0200

    started adding drift

--- src/insane/ll_gbmpb.jl
+++ src/insane/ll_gbmpb.jl
@@ -90,123 +90,6 @@ end
 
 
 """
-    llr_gbm_b(lλp ::Array{Float64,1},
-              lλc ::Array{Float64,1},
-              σλ  ::Float64, 
-              δt  ::Float64,
-              fdt::Float64,
-              srδt::Float64)
-
-Returns the log-likelihood ratio for a branch according to GBM pure-birth.
-"""
-function llr_gbm_b(lλp ::Array{Float64,1},
-                   lλc ::Array{Float64,1},
-                   σλ  ::Float64, 
-                   δt  ::Float64,
-                   fdt ::Float64,
-                   srδt::Float64,
-                   λev ::Bool)
-
-  @inbounds @fastmath begin
-
-    # estimate standard `δt` likelihood
-    nI = lastindex(lλp)-2
-
-    llbm = 0.0
-    llpb = 0.0
-    lλpi = lλp[1]
-    lλci = lλc[1]
-    @simd for i in Base.OneTo(nI)
-      lλpi1 = lλp[i+1]
-      lλci1 = lλc[i+1]
-      llbm += (lλpi1 - lλpi)^2 - (lλci1 - lλci)^2
-      llpb += exp(0.5*(lλpi + lλpi1)) - exp(0.5*(lλci + lλci1))
-      lλpi  = lλpi1
-      lλci  = lλci1
-    end
-
-    # add to global likelihood
-    llr  = llbm*(-0.5/((σλ*srδt)^2))
-    llr -= δt*llpb
-
-    # add final non-standard `δt`
-    if !iszero(fdt)
-      lλpi1 = lλp[nI+2]
-      lλci1 = lλc[nI+2]
-      llr += lrdnorm_bm_x(lλpi1, lλpi, lλci1, lλci, sqrt(fdt)*σλ)
-      llr -= fdt*(exp(0.5*(lλpi + lλpi1)) - exp(0.5*(lλci + lλci1)))
-      if λev
-        llr += lλpi1 - lλci1
-      end
-    end
-  end
-
-  return llr
-end
-
-
-
-
-"""
-    ll_gbm_b_sep(lλv ::Array{Float64,1},
-                 σλ  ::Float64, 
-                 δt  ::Float64,
-                 fdt::Float64,
-                 srδt::Float64)
-
-Returns the log-likelihood for a branch according to GBM pure-birth 
-separately for the Brownian motion and the pure-birth
-"""
-function ll_gbm_b_sep(lλv ::Array{Float64,1},
-                      σλ  ::Float64, 
-                      δt  ::Float64,
-                      fdt ::Float64,
-                      srδt::Float64,
-                      λev ::Bool)
-
-  @inbounds @fastmath begin
-
-    # estimate standard `δt` likelihood
-    nI = lastindex(lλv)-2
-
-    llbm = 0.0
-    llpb = 0.0
-    lλvi = lλv[1]
-    @simd for i in Base.OneTo(nI)
-      lλvi1 = lλv[i+1]
-      llbm += (lλvi1 - lλvi)^2
-      llpb += exp(0.5*(lλvi + lλvi1))
-      lλvi  = lλvi1
-    end
-
-    # add to global likelihood
-    llbm *= (-0.5/((σλ*srδt)^2))
-    llbm -= Float64(nI)*(log(σλ*srδt) + 0.5*log(2.0π))
-    llpb *= (-δt)
-
-    # add final non-standard `δt`
-    lλvi1 = lλv[nI+2]
-    llbm += ldnorm_bm(lλvi1, lλvi, sqrt(fdt)*σλ)
-    llpb -= fdt*exp(0.5*(lλvi + lλvi1))
-
-        # add final non-standard `δt`
-    if !iszero(fdt)
-      lλvi1 = lλv[nI+2]
-      llbm += ldnorm_bm(lλvi1, lλvi, sqrt(fdt)*σλ)
-      llpb -= fdt*exp(0.5*(lλvi + lλvi1))
-      if λev
-        llpb += lλvi1
-      end
-    end
-  end
-
-  return llbm, llpb
-end
-
-
-
-
-"""
     llr_gbm_b_sep(lλp ::Array{Float64,1},
                   lλc ::Array{Float64,1},
                   σλ  ::Float64, 
@@ -219,12 +102,12 @@ Returns the log-likelihood for a branch according to GBM pure-birth
 separately for the Brownian motion and the pure-birth
 """
 @inline function llr_gbm_b_sep(lλp ::Array{Float64,1},
-                       lλc ::Array{Float64,1},
-                       σλ  ::Float64, 
-                       δt  ::Float64,
-                       fdt ::Float64,
-                       srδt::Float64,
-                       λev ::Bool)
+                               lλc ::Array{Float64,1},
+                               σλ  ::Float64, 
+                               δt  ::Float64,
+                               fdt ::Float64,
+                               srδt::Float64,
+                               λev ::Bool)
 
   @inbounds begin
 
@@ -262,123 +145,6 @@ separately for the Brownian motion and the pure-birth
 
   return llrbm, llrpb
 end
-
-
-
-
-"""
-    ll_gbm_b_pb(lλv ::Array{Float64,1},
-                δt  ::Float64,
-                fdt::Float64)
-
-Returns the log-likelihood for a branch according to GBM pure-birth 
-separately for the Brownian motion and the pure-birth
-"""
-@inline function ll_gbm_b_pb(lλv::Array{Float64,1},
-                     δt ::Float64,
-                     fdt::Float64,
-                     λev::Bool)
-
-  @inbounds begin
-
-    # estimate standard `δt` likelihood
-    nI = lastindex(lλv)-2
-
-    ll = 0.0
-    lλvi = lλv[1]
-    @simd for i in Base.OneTo(nI)
-      lλvi1 = lλv[i+1]
-      ll   += exp(0.5*(lλvi + lλvi1))
-      lλvi  = lλvi1
-    end
-
-    # add to global likelihood
-    ll *= (-δt)
-
-    # add final non-standard `δt`
-    if !iszero(fdt)
-      ll -= fdt*exp(0.5*(lλvi + lλv[nI+2]))
-      if λev
-        ll += lλvi1
-      end
-    end
-  end
-
-  return ll
-end
-
-
-
-
-"""
-    llr_gbm_bm(tree::iTgbmpb, 
-               σλp  ::Float64,
-               σλc  ::Float64,
-               srδt::Float64)
-
-Returns the log-likelihood ration for a `iTgbmpb` according 
-to GBM pure-birth for a `σ` proposal.
-"""
-function llr_gbm_bm(tree::iTgbmpb, 
-                    σλp  ::Float64,
-                    σλc  ::Float64,
-                    srδt::Float64)
-
-  if istip(tree) 
-    llr_gbm_bm(lλ(tree), σλp, σλc, fdt(tree), srδt)
-  else
-    llr_gbm_bm(lλ(tree), σλp, σλc, fdt(tree), srδt) +
-    llr_gbm_bm(tree.d1::iTgbmpb, σλp, σλc, srδt)   +
-    llr_gbm_bm(tree.d2::iTgbmpb, σλp, σλc, srδt)
-  end
-end
-
-
-
-
-"""
-    llr_gbm_bm(lv  ::Array{Float64,1},
-               σp  ::Float64, 
-               σc  ::Float64, 
-               fdt::Float64,
-               srδt::Float64)
-
-Returns the log-likelihood ratio for a branch according to BM for `σ` proposal.
-"""
-@inline function llr_gbm_bm(lv  ::Array{Float64,1},
-                            σp  ::Float64, 
-                            σc  ::Float64, 
-                            fdt::Float64,
-                            srδt::Float64)
-
-  @inbounds begin
-
-    # estimate standard `δt` likelihood
-    nI = lastindex(lv)-2
-
-    ss  = 0.0
-    lvi = lv[1]
-    @simd for i in Base.OneTo(nI)
-      lvi1 = lv[i+1]
-      ss  += (lvi1 - lvi)^2
-      lvi  = lvi1
-    end
-
-    # likelihood ratio
-    llr = ss*(0.5/((σc*srδt)^2) - 0.5/((σp*srδt)^2)) - 
-          Float64(nI)*(log(σp/σc))
-
-    # add final non-standard `δt`
-    if !iszero(fdt)
-      srfdt = sqrt(fdt)
-      llr  += lrdnorm_bm_σ(lv[nI+2], lvi, srfdt*σp, srfdt*σc)
-    end
-  end
-
-  return llr
-end
-
-
 
 
 
@@ -455,23 +221,20 @@ end
 
 
 """
-    llr_gbm_bm(σλp ::Float64, 
-               σμp ::Float64, 
-               σλc ::Float64, 
-               σμc ::Float64,
-               sssλ::FLoat64,
-               sssμ::FLoat64,
-               n   ::Float64)
+    llr_gbm_σp(σλp::Float64,
+               σλc::Float64,
+               ssλ::Float64,
+               n  ::Float64)
 
 Returns the log-likelihood ratio according to GBM for `σ` proposal
 after computing standard sum of squares `sss`.
 """
-function llr_gbm_σp(σλp ::Float64,
-                    σλc ::Float64,
-                    sssλ::Float64,
-                    n   ::Float64)
+function llr_gbm_σp(σλp::Float64,
+                    σλc::Float64,
+                    ssλ::Float64,
+                    n  ::Float64)
 
-  llr = sssλ*(1.0/σλc^2 - 1.0/σλp^2) - n*(log(σλp/σλc))
+  llr = ssλ*(1.0/σλc^2 - 1.0/σλp^2) - n*(log(σλp/σλc))
 
   return llr
 end
