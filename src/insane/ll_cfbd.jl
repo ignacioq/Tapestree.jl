96a1fd7e0a335e70ae7c9cb19b5ba405107a9710
Ignacio Quintero <ignacioquintero@gmail.com>
2022-03-02 15:13:20 +0100

    now we assuem that there is always a defined daughter 1 for sampled ancestor tree

--- src/insane/ll_cfbd.jl
+++ src/insane/ll_cfbd.jl
@@ -31,7 +31,7 @@ function llik_cfbd(tree::sTfbd, λ::Float64, μ::Float64, ψ::Float64)
     end
   elseif issampledancestor(tree)
     return - e(tree)*(λ + μ + ψ) + log(ψ) +
-             (def1(tree) ? llik_cfbd(tree.d1::sTfbd, λ, μ, ψ) : 0.0)
+             llik_cfbd(tree.d1::sTfbd, λ, μ, ψ)
   else
     return - e(tree)*(λ + μ + ψ) + log(λ) +
              llik_cfbd(tree.d1::sTfbd, λ, μ, ψ) + 
