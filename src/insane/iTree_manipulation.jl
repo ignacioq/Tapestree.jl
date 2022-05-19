fc0caa95a93ffd18b6943abd57ef9852e32972ce
Ignacio Quintero <ignacioquintero@gmail.com>
2022-05-19 19:20:40 +0200

    added remove extinct for bdX

--- src/insane/iTree_manipulation.jl
+++ src/insane/iTree_manipulation.jl
@@ -1966,6 +1966,88 @@ end
 
 
 """
+    _remove_extinct!(tree::iTbdX)
+
+Remove extinct tips from `iTbdX`.
+"""
+function _remove_extinct!(tree::iTbdX)
+
+  if def1(tree)
+
+    tree.d1 = _remove_extinct!(tree.d1)
+    tree.d2 = _remove_extinct!(tree.d2)
+
+    if isextinct(tree.d1)
+      if isextinct(tree.d2)
+        return iTbdX(e(tree), dt(tree), fdt(tree),
+          true, isfix(tree), lλ(tree), lμ(tree), xv(tree))
+      else
+        ne  = e(tree) + e(tree.d2)
+        lλ0 = lλ(tree)
+        lμ0 = lμ(tree)
+        xv0 = xv(tree)
+        lλ2 = lλ(tree.d2)
+        lμ2 = lμ(tree.d2)
+        xv2 = xv(tree.d2)
+        fdt2 = fdt(tree.d2)
+        pop!(lλ0)
+        pop!(lμ0)
+        pop!(xv0)
+        if iszero(fdt2)
+          popfirst!(lλ2)
+          popfirst!(lμ2)
+          popfirst!(xv2)
+        end
+        prepend!(lλ2, lλ0)
+        prepend!(lμ2, lμ0)
+        prepend!(xv2, xv0)
+
+        fdt0 = fdt(tree) + fdt(tree.d2)
+        if fdt0 > dt(tree)
+          fdt0 -= dt(tree)
+        end
+        tree = tree.d2
+        sete!(tree, ne)
+        setfdt!(tree, fdt0)
+      end
+    elseif isextinct(tree.d2)
+      ne = e(tree) + e(tree.d1)
+      lλ0 = lλ(tree)
+      lμ0 = lμ(tree)
+      xv0 = xv(tree)
+      lλ1 = lλ(tree.d1)
+      lμ1 = lμ(tree.d1)
+      xv1 = xv(tree.d1)
+      fdt1 = fdt(tree.d1)
+      pop!(lλ0)
+      pop!(lμ0)
+      pop!(xv0)
+      if iszero(fdt1)
+        popfirst!(lλ1)
+        popfirst!(lμ1)
+        popfirst!(xv1)
+      end
+      prepend!(lλ1, lλ0)
+      prepend!(lμ1, lμ0)
+      prepend!(xv1, xv0)
+
+      fdt0 = fdt(tree) + fdt(tree.d1)
+      if fdt0 > dt(tree)
+        fdt0 -= dt(tree)
+      end
+      tree = tree.d1
+      sete!(tree, ne)
+      setfdt!(tree, fdt0)
+    end
+  end
+
+  return tree
+end
+
+
+
+
+"""
     remove_fossils(tree::T) where {T <: iTf}
 
 Remove fossils.
