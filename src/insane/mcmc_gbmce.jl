f36fc0a40ac614097aa2a8a882ad32fe990ef117
Ignacio Quintero <ignacioquintero@gmail.com>
2022-06-06 18:27:47 +0200

    down to 1000

--- src/insane/mcmc_gbmce.jl
+++ src/insane/mcmc_gbmce.jl
@@ -86,7 +86,7 @@ function insane_gbmce(tree    ::sT_label,
   else
     λc, μc = λi, μi
   end
-  mc = m_surv_gbmce(th, log(λc), αi, σλi, μc, δt, srδt, 15_000, stem)
+  mc = m_surv_gbmce(th, log(λc), αi, σλi, μc, δt, srδt, 1_000, stem)
 
   # make a decoupled tree
   Ξ = make_Ξ(idf, log(λc), αi, σλi, δt, srδt, iTce)
@@ -835,7 +835,7 @@ function update_α!(αc     ::Float64,
   rs  = σλ2/τ2
   αp  = rnorm((dλ + rs*ν)/(rs + L), sqrt(σλ2/(rs + L)))
 
-  mp  = m_surv_gbmce(th, λ0, αp, σλ, μ, δt, srδt, 15_000, stem)
+  mp  = m_surv_gbmce(th, λ0, αp, σλ, μ, δt, srδt, 1_000, stem)
   llr = log(mp/mc)
 
   if -randexp() < llr
@@ -892,7 +892,7 @@ function update_σ!(σλc     ::Float64,
   σλp2 = randinvgamma(σλ_p1 + 0.5 * n, σλ_p2 + ssλ)
   σλp  = sqrt(σλp2)
 
-  mp  = m_surv_gbmce(th, λ0, α, σλp, μ, δt, srδt, 15_000, stem)
+  mp  = m_surv_gbmce(th, λ0, α, σλp, μ, δt, srδt, 1_000, stem)
   llr = log(mp/mc)
 
   if -randexp() < llr
@@ -943,7 +943,7 @@ function update_μ!(μc     ::Float64,
 
   μp  = randgamma(μ_prior[1] + ne, μ_prior[2] + L)
 
-  mp  = m_surv_gbmce(th, λ0, α, σλ, μp, δt, srδt, 15_000, stem)
+  mp  = m_surv_gbmce(th, λ0, α, σλ, μp, δt, srδt, 1_000, stem)
   llr = log(mp/mc)
 
   if -randexp() < llr
--- src/insane/mhprop_gbmce.jl
+++ src/insane/mhprop_gbmce.jl
@@ -125,7 +125,7 @@ function _stem_update!(ξi   ::iTce,
     llrbm, llrbd, ssrλ = llr_gbm_b_sep(λp, λc, α, σλ, δt, fdtp, srδt, false)
 
     # survival
-    mp  = m_surv_gbmce(th, λr, α, σλ, μ, δt, srδt, 15_000, true)
+    mp  = m_surv_gbmce(th, λr, α, σλ, μ, δt, srδt, 1_000, true)
     llr = log(mp/mc)
 
     acr = llrbd + llr
@@ -213,7 +213,7 @@ function _crown_update!(ξi   ::iTce,
       llr_gbm_b_sep(λ2p, λ2c, α, σλ, δt, fdt2, srδt, false)
 
     # survival
-    mp  = m_surv_gbmce(th, λr, α, σλ, μ, δt, srδt, 15_000, false)
+    mp  = m_surv_gbmce(th, λr, α, σλ, μ, δt, srδt, 1_000, false)
     llr = log(mp/mc)
 
     acr = llrce1 + llrce2 + llr 
