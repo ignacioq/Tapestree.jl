2bf92f64374507a8c52ea82b166fea78f4222733
Ignacio Quintero <ignacioquintero@gmail.com>
2022-09-06 09:55:19 +0200

    added constant change fucntion

--- src/Tapestree.jl
+++ src/Tapestree.jl
@@ -44,7 +44,7 @@ using .INSANE: read_newick, write_newick,
   insane_gbmpb, insane_gbmce, insane_gbmct, insane_gbmbd, insane_gbmfbd,
   iquantile, imean, irange, extract_vector!, mcmc_array, lλ, lμ, 
   remove_extinct, remove_unsampled, remove_fossils, fixedpos,
-  b, d, ld, lb, nd, t, lt, dλ, dμ,
+  b, d, ld, lb, nd, t, lt, dλ, dμ, dλc, dμc,
   ntipsalive, ntips, ntipsextinct, sustainedcount,
   treeheight, treelength, _ctl, ltt, subclade, tiplabels, time_rate
 export read_newick, write_newick, 
@@ -56,7 +56,7 @@ export read_newick, write_newick,
   insane_gbmpb, insane_gbmce, insane_gbmct, insane_gbmbd, insane_gbmfbd,
   iquantile, imean, irange, extract_vector!, mcmc_array, lλ, lμ, 
   remove_extinct, remove_unsampled, remove_fossils, fixedpos,
-  b, d, ld, lb, nd, t, lt, dλ, dμ,
+  b, d, ld, lb, nd, t, lt, dλ, dμ, dλc, dμc,
   ntipsalive, ntips, ntipsextinct, sustainedcount,
   treeheight, treelength, _ctl, ltt, subclade, tiplabels, time_rate
 
