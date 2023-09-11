11cbba2fcae8f1b8f24204045581638d259b65b6
Jérémy Andréoletti <45073058+Jeremy-Andreoletti@users.noreply.github.com>
2023-09-11 10:41:41 +0200

    Joint CFBD likelihood and LTT

--- src/insane/ll_cfbd.jl
+++ src/insane/ll_cfbd.jl
@@ -123,3 +123,70 @@ end
 
 
 
+
+"""
+    _llik_cfbd_eventimes!(tree::sTfbd,
+                          λ   ::Float64,
+                          μ   ::Float64,
+                          ψ   ::Vector{Float64},
+                          t   ::Float64,
+                          ψts ::Vector{Float64},
+                          ix  ::Int64,
+                          nep ::Int64, 
+                          se  ::Vector{Float64}, 
+                          ee  ::Vector{Float64})
+
+Log-likelihood up to a constant for piecewise-constant fossilized birth-death
+given a complete `iTree` recursively, while recording the times of speciations
+and extinctions in order to construct the LTT.
+"""
+function _llik_cfbd_eventimes!(tree::sTfbd,
+                               λ   ::Float64,
+                               μ   ::Float64,
+                               ψ   ::Vector{Float64},
+                               t   ::Float64,
+                               ψts ::Vector{Float64},
+                               ix  ::Int64,
+                               nep ::Int64,
+                               se  ::Vector{Float64}, 
+                               ee  ::Vector{Float64})
+  @inbounds begin
+
+    ei  = e(tree)
+    ll = 0.0
+    # if epoch change
+    while ix < nep && t - ei < ψts[ix]
+      li   = t - ψts[ix]
+      ll  -= li*(λ + μ + ψ[ix])
+      ei  -= li
+      t    = ψts[ix]
+      ix  += 1
+    end
+
+    ll -= ei*(λ + μ + ψ[ix])
+    t  -= ei
+
+    if def1(tree)
+      if def2(tree)
+        push!(se, t)
+        ll += log(λ)                                              +
+              _llik_cfbd_eventimes!(tree.d1::sTfbd, λ, μ, ψ, t, ψts, ix, nep, se, ee) +
+              _llik_cfbd_eventimes!(tree.d2::sTfbd, λ, μ, ψ, t, ψts, ix, nep, se, ee)
+      else
+        ll += log(ψ[ix])                                          +
+              _llik_cfbd_eventimes!(tree.d1::sTfbd, λ, μ, ψ, t, ψts, ix, nep, se, ee)
+      end
+    else
+      if isextinct(tree)
+        push!(ee, t)
+        ll += log(μ)
+      end
+      ll += (isfossil(tree) ? log(ψ[ix]) : 0.0)
+    end
+  end
+
+  return ll
+end
+
+
+
