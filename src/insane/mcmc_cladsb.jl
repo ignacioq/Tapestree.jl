450b60a3e6c7dc9f892977f1c25923330b359caf
Ignacio Quintero <ignacioquintero@gmail.com>
2025-09-04 15:59:28 +0200

    updated doc

--- src/insane/mcmc_cladsb.jl
+++ src/insane/mcmc_cladsb.jl
@@ -14,39 +14,38 @@ Created 14 09 2020
 
 """
     insane_cladsb(tree    ::sT_label;
-                 λ0_prior::NTuple{2,Float64}     = (0.05, 148.41),
-                 α_prior ::NTuple{2,Float64}     = (0.0, 1.0),
-                 σλ_prior::NTuple{2,Float64}     = (0.05, 0.05),
-                 niter   ::Int64                 = 1_000,
-                 nthin   ::Int64                 = 10,
-                 nburn   ::Int64                 = 200,
-                 nflush  ::Int64                 = nthin,
-                 ofile   ::String                = string(homedir(), "/ipb"),
-                 αi      ::Float64               = 0.0,
-                 σλi     ::Float64               = 0.1,
-                 pupdp   ::NTuple{5,Float64}     = (1e-3, 1e-3, 1e-3, 0.2, 0.2),
-                 δt      ::Float64               = 1e-3,
-                 prints  ::Int64                 = 5,
-                 stn     ::Float64               = 0.5,
-                 tρ      ::Dict{String, Float64} = Dict("" => 1.0))
+                  λ0_prior::NTuple{2,Float64}     = (0.05, 148.41),
+                  α_prior ::NTuple{2,Float64}     = (0.0, 1.0),
+                  σλ_prior::NTuple{2,Float64}     = (0.05, 0.05),
+                  niter   ::Int64                 = 1_000,
+                  nthin   ::Int64                 = 10,
+                  nburn   ::Int64                 = 200,
+                  nflush  ::Int64                 = nthin,
+                  ofile   ::String                = string(homedir(), "/cladsb"),
+                  αi      ::Float64               = 0.0,
+                  σλi     ::Float64               = 0.1,
+                  pupdp   ::NTuple{5,Float64}     = (1e-3, 1e-3, 1e-4, 0.1, 0.2),
+                  prints  ::Int64                 = 5,
+                  stn     ::Float64               = 0.5,
+                  tρ      ::Dict{String, Float64} = Dict("" => 1.0))
 
 Run insane for clads `pd`.
 """
 function insane_cladsb(tree    ::sT_label;
-                        λ0_prior::NTuple{2,Float64}     = (0.05, 148.41),
-                        α_prior ::NTuple{2,Float64}     = (0.0, 1.0),
-                        σλ_prior::NTuple{2,Float64}     = (0.05, 0.05),
-                        niter   ::Int64                 = 1_000,
-                        nthin   ::Int64                 = 10,
-                        nburn   ::Int64                 = 200,
-                        nflush  ::Int64                 = nthin,
-                        ofile   ::String                = string(homedir(), "/cladsb"),
-                        αi      ::Float64               = 0.0,
-                        σλi     ::Float64               = 0.1,
-                        pupdp   ::NTuple{5,Float64}     = (1e-3, 1e-3, 1e-4, 0.1, 0.2),
-                        prints  ::Int64                 = 5,
-                        stn     ::Float64               = 0.5,
-                        tρ      ::Dict{String, Float64} = Dict("" => 1.0))
+                       λ0_prior::NTuple{2,Float64}     = (0.05, 148.41),
+                       α_prior ::NTuple{2,Float64}     = (0.0, 1.0),
+                       σλ_prior::NTuple{2,Float64}     = (0.05, 0.05),
+                       niter   ::Int64                 = 1_000,
+                       nthin   ::Int64                 = 10,
+                       nburn   ::Int64                 = 200,
+                       nflush  ::Int64                 = nthin,
+                       ofile   ::String                = string(homedir(), "/cladsb"),
+                       αi      ::Float64               = 0.0,
+                       σλi     ::Float64               = 0.1,
+                       pupdp   ::NTuple{5,Float64}     = (1e-3, 1e-3, 1e-4, 0.1, 0.2),
+                       prints  ::Int64                 = 5,
+                       stn     ::Float64               = 0.5,
+                       tρ      ::Dict{String, Float64} = Dict("" => 1.0))
 
   n    = ntips(tree)
   th   = treeheight(tree)
